c2a2f8a429c2aaa6d44b2b3dc057a3a0fe053b5e
零下乆 <x13178463260@outlook.com>
2025-02-23 19:30:25 +0800

    提交框架

---
+++ aspnet-core/src/HIS.Application/HISApplicationAutoMapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace HIS;
+
+public class HISApplicationAutoMapperProfile : Profile
+{
+    public HISApplicationAutoMapperProfile()
+    {
+        /* You can configure your AutoMapper mapping configuration here.
+         * Alternatively, you can split your mapping configurations
+         * into multiple profile classes for a better organization. */
+    }
+}
