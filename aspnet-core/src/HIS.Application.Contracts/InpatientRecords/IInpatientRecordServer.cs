6209a9dde2542b1e91bdbdcaa3c2fa266a080775
weijinlei <3135927105@qq.com>
2025-02-27 09:22:26 +0800

    1

---
+++ aspnet-core/src/HIS.Application.Contracts/InpatientRecords/IInpatientRecordServer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace HIS.InpatientRecords
+{
+    public interface IInpatientRecordServer : IApplicationService
+    {
+        Task<APIResult<InpatientRecordDto>> AddInpatientRecord(InpatientRecordDto patient);
+    }
+}
