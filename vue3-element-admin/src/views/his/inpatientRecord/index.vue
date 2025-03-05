eee1222f1a331b278c58a64ef10e744b61774c91
零下乆 <x13178463260@outlook.com>
2025-03-05 20:19:46 +0800

    123

--- vue3-element-admin/src/views/his/inpatientRecord/index.vue
+++ vue3-element-admin/src/views/his/inpatientRecord/index.vue
@@ -1,35 +1,35 @@
 <template>
   <el-form-item>
-      <el-button type="primary" @click="dialogVisible = true">新增住院</el-button>
-    </el-form-item>
+    <el-button type="primary" @click="dialogVisible = true">新增住院</el-button>
+  </el-form-item>
 
   <el-table :data="tableData" border style="width: 100%">
     <el-table-column prop="patient_name" label="患者姓名" />
-    <el-table-column prop="admission_date" label="入院时间" >
-       <template #default="{ row }">
+    <el-table-column prop="admission_date" label="入院时间">
+      <template #default="{ row }">
         <span>{{ row.admission_date.slice(0, 10) }}</span>
       </template>
     </el-table-column>
-    <el-table-column prop="discharge_date" label="出院时间" >
-       <template #default="{ row }">
+    <el-table-column prop="discharge_date" label="出院时间">
+      <template #default="{ row }">
         <span>{{ row.discharge_date.slice(0, 10) }}</span>
       </template>
     </el-table-column>
     <el-table-column prop="department_name" label="科室名称" />
     <el-table-column prop="doctor_name" label="主治医生姓名" />
-    <el-table-column prop="room_type" label="病房类型" />  
+    <el-table-column prop="room_type" label="病房类型" />
     <el-table-column prop="admission_reason" label="入院信息" />
-    <el-table-column prop="is_in_insurance" label="是否住院" >
+    <el-table-column prop="is_in_insurance" label="是否住院">
       <template #default="{ row }">
         <span v-if="row.card_status == '在院'">在院</span>
         <span v-else>离院</span>
       </template>
     </el-table-column>
-    <el-table-column label="操作" >
+    <el-table-column label="操作">
       <template #default="{ row }">
         <el-button type="text" size="small" @click="delshow(row)">删除</el-button>
       </template>
-      </el-table-column>
+    </el-table-column>
   </el-table>
   <el-dialog v-model="dialogVisible" title="新增住院" width="500" draggable overflow>
     <span>基本信息</span>
@@ -40,20 +40,12 @@
         </el-select>
       </el-form-item>
       <el-form-item label="入院时间">
-        <el-date-picker
-          v-model="InpatientRecordInfor.admission_date"
-          type="date"
-          placeholder="Pick a date"
-          style="width: 100%"
-        />
+        <el-date-picker v-model="InpatientRecordInfor.admission_date" type="date" placeholder="Pick a date"
+          style="width: 100%" />
       </el-form-item>
       <el-form-item label="出院时间">
-        <el-date-picker
-          v-model="InpatientRecordInfor.discharge_date"
-          type="date"
-          placeholder="Pick a date"
-          style="width: 100%"
-        />
+        <el-date-picker v-model="InpatientRecordInfor.discharge_date" type="date" placeholder="Pick a date"
+          style="width: 100%" />
       </el-form-item>
       <el-form-item label="科室名称">
         <el-select v-model="InpatientRecordInfor.department_id">
@@ -69,13 +61,13 @@
         <el-input v-model="InpatientRecordInfor.room_type" placeholder="请输入病房类型" clearable />
       </el-form-item>
       <el-form-item label="入院信息">
-        <el-input type="textarea" v-model="InpatientRecordInfor.admission_reason" placeholder="请输入院信息" clearable />
+        <el-input v-model="InpatientRecordInfor.admission_reason" type="textarea" placeholder="请输入院信息" clearable />
       </el-form-item>
       <el-form-item label="是否住院">
         <el-radio-group v-model="InpatientRecordInfor.is_in_insurance">
-        <el-radio value="在院">在院</el-radio>
-        <el-radio value="离院">离院</el-radio>
-      </el-radio-group>
+          <el-radio value="在院">在院</el-radio>
+          <el-radio value="离院">离院</el-radio>
+        </el-radio-group>
       </el-form-item>
     </el-form>
     <template #footer>
@@ -88,35 +80,34 @@
 </template>
 
 <script lang="ts" setup>
-import inpatientRecordAPI, { InpatientRecordDto, inpatientRecordAPIQuery, InpatientRecordInfor } from "@/api/his/inpatientRecord/index";
+import inpatientRecordAPI, {
+  InpatientRecordDto,
+  inpatientRecordAPIQuery,
+  InpatientRecordInfor,
+} from "@/api/his/inpatientRecord/index";
 //删除弹出框
 const delshow = (id: string) => {
-  ElMessageBox.confirm(
-    '确定要删除吗?',
-    '警告',
-    {
-      confirmButtonText: 'OK',
-      cancelButtonText: '取消',
-      type: 'warning',
-    }
-  )
+  ElMessageBox.confirm("确定要删除吗?", "警告", {
+    confirmButtonText: "OK",
+    cancelButtonText: "取消",
+    type: "warning",
+  })
     .then(() => {
-      inpatientRecordAPI.delList(id).then(() => {   
-        debugger
+      inpatientRecordAPI.delList(id).then(() => {
         ElMessage({
-          type: 'success',
-          message: '删除成功!',
-        })
+          type: "success",
+          message: "删除成功!",
+        });
         loadlist();
-      })
+      });
     })
     .catch(() => {
       ElMessage({
-        type: 'info',
-        message: '已取消删除',
-      })
-    })
-}
+        type: "info",
+        message: "已取消删除",
+      });
+    });
+};
 //对话框
 const dialogVisible = ref(false);
 // const deptlist = ref<GetDepartmentDto[]>([]);
@@ -129,11 +120,11 @@ const inserttInpatientRecordInfor = ref<InpatientRecordInfor>({
   id: "",
   concurrencyStamp: "",
   creationTime: "",
-  creatorId: "" ,
+  creatorId: "",
   lastModificationTime: "",
   lastModifierId: "",
   isDeleted: false,
-  deleterId:"",
+  deleterId: "",
   deletionTime: "",
   patient_id: "",
   patient_name: "",
@@ -145,8 +136,8 @@ const inserttInpatientRecordInfor = ref<InpatientRecordInfor>({
   doctor_name: "",
   room_type: "",
   admission_reason: "",
-  is_in_insurance: false
-})
+  is_in_insurance: false,
+});
 //添加住院信息
 const insert = () => {
   inpatientRecordAPI.addList(inserttInpatientRecordInfor.value).then(() => {
@@ -158,12 +149,12 @@ const tableData = ref<InpatientRecordDto[]>([]);
 
 const formInline = ref<inpatientRecordAPIQuery>({});
 const loadlist = () => {
-   inpatientRecordAPI.getList(formInline.value).then((res) => {
+  inpatientRecordAPI.getList(formInline.value).then((res) => {
     tableData.value = res;
   });
-}
+};
 /** 页面加载完成后加载数据 */
 onMounted(() => {
- loadlist();
+  loadlist();
 });
 </script>
